43f1b4c80e709b5d0c11cae4c4031bd145da189e
Vasiliy <kirstiavasilii@gmail.com>
2015-02-02 22:24:48 +0200

    Доработано:  - регистрация пользователей работоспособна на 100%  - отображение имени автора и дат

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,7 @@ from account.views import *
 
 urlpatterns = patterns(
     '',
-    url(r'^registration/$', RegistrationUser.as_view(), name='registration'),
+    url(r'^registration/$', RegistrationView.as_view(), name='registration'),
     url(r'^login/$', log_in, name='login'),
     url(r'^logout/$', log_out, name='logout'),
 )
