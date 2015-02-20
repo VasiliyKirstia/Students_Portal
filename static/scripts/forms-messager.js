5f940af6d87e5fcc999238a739781861253ce292
Vasiliy <kirstiavasilii@gmail.com>
2015-02-20 16:51:07 +0200

    Добавил предупреждающее сообщение при загрузке файлов на сервер.

--- static/scripts/forms-messager.js
+++ static/scripts/forms-messager.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $('#form_with_big_data').submit(function(eventObj){
-        alert('Pleas, wait. Your data sending now.');
+        $('#loading').show();
     });
 });
