f21e970fede24e94357ddcfb0335a5b444057ca9
Vasiliy <kirstiavasilii@gmail.com>
2015-04-16 09:31:39 +0300

    поправил модели.

--- chat/models.py
+++ chat/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.html import escape
 
-class Room(models.Model):
 
+class Room(models.Model):
     id = models.AutoField(primary_key=True)
 
     title = models.CharField(max_length=150, verbose_name='название')
@@ -12,41 +12,31 @@ class Room(models.Model):
                                      through='Membership',
                                      verbose_name='участники')
 
-    last_recv = models.DateTimeField(auto_now=True)
-
-    def __add_message(self, type, sender, message=None):
-        """Generic function for adding a message to the chat room"""
-        m = Message(room=self, type=type, author=sender, message=escape(message))
+    def __add_message(self, sender, message):
+        m = Message(room=self, author=sender, message=escape(message))
         m.save()
         return m
 
     def say(self, sender, message):
-        """Say something in to the chat room"""
-        return self.__add_message('m', sender, message)
+        return self.__add_message(sender, message)
 
     def join(self, user):
-        """A user has joined"""
         Membership.objects.create(room=self, user=user)
-        if Membership.objects.filter(room=self).filter(user=user).count() == 1:
-            return self.__add_message('j', user)
 
     def leave(self, user):
-        """A user has leaved"""
         Membership.objects.filter(room=self).filter(user=user)[0].delete()
-        if Membership.objects.filter(room=self).filter(user=user).count() == 0:
-            return self.__add_message('l', user)
 
     def messages(self, after_pk=None, after_date=None):
-        """List messages, after the given id or date"""
         m = Message.objects.filter(room=self)
         if after_pk:
             m = m.filter(pk__gt=after_pk)
+            return m.order_by('pk')
         if after_date:
             m = m.filter(timestamp__gte=after_date)
-        return m.order_by('pk')
+            return m.order_by('timestamp')
+
 
     def last_message_id(self):
-        """Return last message sent to room"""
         m = Message.objects.filter(room=self).order_by('pk')
         if m:
             return m[0].id - 1
@@ -54,10 +44,9 @@ class Room(models.Model):
             return 0
 
     def __str__(self):
-        return 'Chat for %s %d' % (self.content_type, self.object_id)
+        return self.title
 
     class Meta:
-        ordering = ['-last_recv']
         verbose_name = 'комната'
         verbose_name_plural = 'комнаты'
 
@@ -67,51 +56,47 @@ class Membership(models.Model):
     user = models.ForeignKey(User)
 
 
-MESSAGE_TYPE_CHOICES = (
-    ('s', 'system'),
-    ('a', 'action'),
-    ('m', 'message'),
-    ('j', 'join'),
-    ('l', 'leave'),
-    ('n', 'notification')
-)
+class Invite(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    room = models.ForeignKey(to=Room,
+                             related_name='invites',
+                             verbose_name='комната')
+
+    to = models.ForeignKey(to=User,
+                           related_name='invites',
+                           verbose_name='приглашенный')
+
+    def to_json(self):
+        return {
+            'title': self.room.title,
+            'room_id': self.room.id,
+        }
+
+    class Meta:
+        verbose_name = 'приглашение'
+        verbose_name_plural = 'приглашения'
 
 
 class Message(models.Model):
-
     id = models.AutoField(primary_key=True)
 
     room = models.ForeignKey(Room, verbose_name='комната')
 
-    type = models.CharField(max_length=1, choices=MESSAGE_TYPE_CHOICES, verbose_name='тип')
+    author = models.ForeignKey(User, related_name='messages', verbose_name='автор')
 
-    author = models.ForeignKey(User, related_name='messages', blank=True, null=True, verbose_name='автор')
-
-    message = models.CharField(max_length=255, blank=True, null=True, verbose_name='текст сообщения')
+    message = models.CharField(verbose_name='текст сообщения')
 
     timestamp = models.DateTimeField(auto_now=True, verbose_name='время отправки')
 
     def to_json(self):
         return {
-            'id': self.pk,
+            'date': self.timestamp,
             'author': "{} {}".format(self.author.first_name, self.author.last_name),
             'message': self.__str__(),
-            'type': self.type
         }
 
     def __str__(self):
-        """Each message type has a special representation, return that representation.
-        This will also be translator AKA i18l friendly."""
-        if self.type == 's':
-            return u'SYSTEM: %s' % self.message
-        if self.type == 'n':
-            return u'NOTIFICATION: %s' % self.message
-        elif self.type == 'j':
-            return 'JOIN: %s' % self.author
-        elif self.type == 'l':
-            return 'LEAVE: %s' % self.author
-        elif self.type == 'a':
-            return 'ACTION: %s > %s' % (self.author, self.message)
         return self.message
 
     class Meta:
